5d52fb8990d834cfc5070c82946d0f36f0c2818e
Amire Joseph <amirejoseph83@gmail.com>
2025-05-23 08:14:41 +0100

    trouble shooting

--- api/index.py
+++ api/index.py
@@ -1,17 +1,9 @@
-from .config.config import (
-    origins,
-    FastAPI,
-    Response,
-    API_V1,
-    HTTPException,
-    status,
-    Request,
-)
+from fastapi import FastAPI, status, HTTPException
 
 app = FastAPI()
 
 
 #  Testing API
-@app.get(f"{API_V1}/", tags=["Test"], description="Route for testing API")
+@app.get("/", tags=["Test"], description="Route for testing API")
 def read_root():
     return HTTPException(status_code=status.HTTP_200_OK, detail="Welcome to Chatbot")
