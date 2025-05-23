a09d8992cbd3d31d590c15c0822337d55ff46aca
Amire Joseph <amirejoseph83@gmail.com>
2025-05-23 08:22:39 +0100

    Added som line of code

--- api/config/config.py
+++ api/config/config.py
@@ -1,1 +1,31 @@
-from fastapi import FastAPI, HTTPException, status
+from fastapi import (
+    FastAPI,
+    APIRouter,
+    Request,
+    Response,
+    File,
+    UploadFile,
+    status,
+    HTTPException,
+)
+from fastapi.responses import JSONResponse
+
+# from imagekitio import ImageKit
+import uuid
+import os
+import json
+import base64
+
+# import multipart
+from dotenv import load_dotenv
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from typing import Dict, Any
+import requests
+from google.oauth2 import service_account
+from google.auth.transport.requests import Request as GoogleRequest
+from google.api_core.exceptions import GoogleAPICallError
+from google.cloud import dialogflow_v2 as dialogflow
+
+
+API_V1 = "/api/v1"
--- api/index.py
+++ api/index.py
@@ -1,9 +1,9 @@
-from .config.config import FastAPI, HTTPException, status
+from .config.config import FastAPI, HTTPException, status, API_V1
 
 app = FastAPI()
 
 
 #  Testing API
-@app.get("/", tags=["Test"], description="Route for testing API")
+@app.get(f"{API_V1}", tags=["Test"], description="Route for testing API")
 def read_root():
     return HTTPException(status_code=status.HTTP_200_OK, detail="Welcome to Chatbot")
